0c366ff3d25575a2a9e67cf2454e5b79be5c6468
xmeix <lamiaboualouache@gmail.com>
2022-05-22 15:38:44 +0100

    adding routes

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -4,19 +4,31 @@ const Header = () => {
   return (
     <div className="header">
       <ul>
-        <Link to="/" style={{ textDecoration: "none", color: "black" }}>
+        <Link
+          to="/lamiaboualouache-portfolio/"
+          style={{ textDecoration: "none", color: "black" }}
+        >
           <li>Home</li>
         </Link>
 
-        <Link to="/Projects" style={{ textDecoration: "none", color: "black" }}>
+        <Link
+          to="/lamiaboualouache-portfolio/Projects"
+          style={{ textDecoration: "none", color: "black" }}
+        >
           <li>Projects</li>
         </Link>
 
-        <Link to="/AboutMe" style={{ textDecoration: "none", color: "black" }}>
+        <Link
+          to="/lamiaboualouache-portfolio/AboutMe"
+          style={{ textDecoration: "none", color: "black" }}
+        >
           <li>AboutMe</li>
         </Link>
 
-        <Link to="/Contact" style={{ textDecoration: "none", color: "black" }}>
+        <Link
+          to="/lamiaboualouache-portfolio/Contact"
+          style={{ textDecoration: "none", color: "black" }}
+        >
           <li>Contact</li>
         </Link>
       </ul>
